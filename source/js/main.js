47d4cc91efd07c650acab7363b822122eba71c78
Danila Petrov <daniilpetrov776@gmail.com>
2024-11-08 16:28:56 +0300

    stage2 ready

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,7 @@
+import './video';
+
 // https://swiperjs.com/get-started#installation
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
---
+++ source/js/video.js
@@ -0,0 +1,26 @@
+const playButton = document.querySelector('.video__play-button');
+
+const playButtonClickHandler = () => {
+  const iframe = document.createElement('iframe');
+  iframe.setAttribute('width', '100%');
+  iframe.setAttribute('height', '100%');
+  iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1');
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('loading', 'lazy');
+
+  const videoFrame = document.querySelector('.video__video-iframe');
+  videoFrame.innerHTML = '';
+  videoFrame.appendChild(iframe);
+  videoFrame.classList.add('video__video-iframe--playing');
+
+  const thumbnail = document.querySelector('.video__preview-wrapper');
+  thumbnail.classList.add('video__preview-wrapper--playing');
+
+  playButton.classList.add('video__play-button--playing');
+};
+
+if (playButton) {
+  playButton.addEventListener('click', playButtonClickHandler);
+}
