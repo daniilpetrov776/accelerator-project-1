bd5f8d7004e39c5ccd0654279d0fc3f26bbf97db
Danila <daniilpetrov776@gmail.com>
2024-11-13 18:28:15 +0300

    swiper for juri

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -9,9 +9,27 @@ const swiper = new Swiper('.swiper', {
     enabled: true,
     onlyInViewport: true,
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      // navigation: false,
+      // spaceBetween: 20
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    // when window width is >= 640px
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  },
 
-  slidesPerView: 4,
-  spaceBetween: 40,
+  // slidesPerView: 4,
+  // spaceBetween: 40,
 
   navigation: {
     nextEl: '.swiper-button-next',
