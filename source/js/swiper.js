142129ffd5539cec2094df7c08c32656e15e5469
Danila <daniilpetrov776@gmail.com>
2024-11-14 18:12:40 +0300

    features WIP

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -10,27 +10,19 @@ const swiper = new Swiper('.swiper', {
     onlyInViewport: true,
   },
   breakpoints: {
-    // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      // navigation: false,
-      // spaceBetween: 20
+      spaceBetween: 10
     },
-    // when window width is >= 480px
     768: {
       slidesPerView: 2,
       spaceBetween: 40
     },
-    // when window width is >= 640px
     1366: {
       slidesPerView: 4,
       spaceBetween: 40
     }
   },
-
-  // slidesPerView: 4,
-  // spaceBetween: 40,
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
