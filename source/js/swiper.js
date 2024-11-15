7eaf225230d969431757ab7bdb7e751d9bc8839c
Danila Petrov <daniilpetrov776@gmail.com>
2024-11-15 16:41:42 +0300

    states for swiper buttons

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -3,7 +3,7 @@ import Swiper from 'swiper/bundle';
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   init: false,
-  loop: true,
+  loop: false,
   simulateTouch: false,
   keyboard: {
     enabled: true,
