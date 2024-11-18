8f9666380a49ffd027cba121e2925e54caf20183
Danila <daniilpetrov776@gmail.com>
2024-11-18 23:08:51 +0300

    working faq tabs and accordeon

--- source/js/main.js
+++ source/js/main.js
@@ -3,13 +3,13 @@ import { LazyLoadVideo } from './video';
 import { displayPrices } from './price';
 import { DEFAULT_SERVICE_TIME } from './const';
 import { swiper } from './swiper';
-import { handleAccButtons } from './accordeon';
+import { updateFaqControls } from './accordeon';
 
 blockDisabledElements();
 LazyLoadVideo();
 displayPrices(DEFAULT_SERVICE_TIME);
 swiper.init();
-handleAccButtons();
+updateFaqControls();
 // https://swiperjs.com/get-started#installation
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
